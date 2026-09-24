f33a7b072a1e0ac56f7297e601712206af9ca48d
hendriyd <hendrieyd14@gmail.com>
2026-09-24 11:35:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ function buildBlocks() {
           <select name="block${i}_propertyType">
             <option value="">Pilih</option>
             <option value="Rumah">Rumah</option>
+             <option value="Apartemen">Apartemen</option>
             <option value="Tanah">Tanah</option>
             <option value="Ruko">Ruko</option>
             <option value="Gedung">Gedung</option>
